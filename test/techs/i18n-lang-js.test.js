f4d3c2952d53cd07b6e682aa6086879587c8dde4
tormozz48 <tormozz48@gmail.com>
2015-05-20 10:47:35 +0300

    Implement tests for cache functionality

--- test/techs/i18n-lang-js.test.js
+++ test/techs/i18n-lang-js.test.js
@@ -1,8 +1,6 @@
-var fs = require('fs'),
-    path = require('path'),
+var path = require('path'),
     mockFs = require('mock-fs'),
     TestNode = require('enb/lib/test/mocks/test-node'),
-    FileList = require('enb/lib/file-list'),
     dropRequireCache = require('enb/lib/fs/drop-require-cache'),
     Tech = require('../../techs/i18n-lang-js');
 
@@ -67,6 +65,81 @@ describe('i18n-lang-js', function () {
                 res.must.be('core');
             });
     });
+
+    describe('cache', function () {
+        it('must get keyset from cache', function () {
+            var time = new Date(1),
+                initialScheme = {
+                    bundle: {
+                        'bundle.keysets.lang.js': mockFs.file({
+                            content: 'module.exports = { scope: { key: "val" } };',
+                            mtime: time
+                        })
+                    }
+                },
+                modifiedScheme = {
+                    bundle: {
+                        'bundle.keysets.lang.js': mockFs.file({
+                            content: 'module.exports = { scope: { key: "val2" } };',
+                            mtime: time
+                        })
+                    }
+                },
+                expected = [
+                    'if (typeof BEM !== \'undefined\' && BEM.I18N) {BEM.I18N.decl(\'scope\', {',
+                    '    "key": \'val\'',
+                    '}, {',
+                    '"lang": "lang"',
+                    '});',
+                    '',
+                    'BEM.I18N.lang(\'lang\');',
+                    '',
+                    '}',
+                    ''
+                ].join('\n');
+
+            return buildWithCache(initialScheme, modifiedScheme, 'lang')
+                .then(function (res) {
+                    res.must.eql(expected);
+                });
+        });
+
+        it('must ignore outdated cache', function () {
+            var initialScheme = {
+                    bundle: {
+                        'bundle.keysets.lang.js': mockFs.file({
+                            content: 'module.exports = { scope: { key: "val" } };',
+                            mtime: new Date(1)
+                        })
+                    }
+                },
+                modifiedScheme = {
+                    bundle: {
+                        'bundle.keysets.lang.js': mockFs.file({
+                            content: 'module.exports = { scope: { key: "val2" } };',
+                            mtime: new Date(2)
+                        })
+                    }
+                },
+                expected = [
+                    'if (typeof BEM !== \'undefined\' && BEM.I18N) {BEM.I18N.decl(\'scope\', {',
+                    '    "key": \'val2\'',
+                    '}, {',
+                    '"lang": "lang"',
+                    '});',
+                    '',
+                    'BEM.I18N.lang(\'lang\');',
+                    '',
+                    '}',
+                    ''
+                ].join('\n');
+
+            return buildWithCache(initialScheme, modifiedScheme, 'lang')
+                .then(function (res) {
+                    res.must.eql(expected);
+                });
+        });
+    });
 });
 
 function build(keysets, lang) {
@@ -85,6 +158,27 @@ function build(keysets, lang) {
     mockFs(fsScheme);
 
     var bundle = new TestNode('bundle');
+
+    return bundle.runTechAndGetContent(Tech, { lang: lang })
+        .spread(function (res) {
+            return res.toString();
+        });
+}
+
+function buildWithCache(initialScheme, modifiedScheme, lang) {
+    mockFs(initialScheme);
+
+    var bundle = new TestNode('bundle'),
+        keySetsFileName = 'bundle.keysets.lang.js',
+        cache = bundle.getNodeCache('bundle.lang.lang.js'),
+        dirname = path.resolve('./bundle'),
+        keysetsFileFullPath = path.join(dirname, keySetsFileName);
+
+    dropRequireCache(require, keysetsFileFullPath);
+    require(keysetsFileFullPath);
+    cache.cacheFileInfo('keysets-file-' + keySetsFileName, keysetsFileFullPath);
+
+    mockFs(modifiedScheme);
 
     return bundle.runTechAndGetContent(Tech, { lang: lang })
         .spread(function (res) {
