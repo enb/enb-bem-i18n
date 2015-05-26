7b5079803d682aef5b742a4aa76716c644249161
tormozz48 <tormozz48@gmail.com>
2015-05-26 13:01:27 +0300

    Merge i18n-merge-keysets and keysets techs between two i18n packages

--- techs/i18n-merge-keysets.js
+++ techs/i18n-merge-keysets.js
@@ -18,64 +18,82 @@
  * ]);
  * ```
  */
+/**
+ * keysets
+ * =======
+ *
+ * Собирает `?.keysets.<язык>.js`-файлы на основе `*.i18n`-папок для указанных языков.
+ *
+ * **Опции**
+ *
+ * * *String* **target** — Результирующий таргет. По умолчанию — `?.keysets.{lang}.js`.
+ * * *String* **lang** — Язык, для которого небходимо собрать файл.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech([require('enb-bem-core-i18n/techs/keysets'), { lang: '{lang}' }]);
+ * ```
+ */
+
 var vow = require('vow'),
+    serialize = require('serialize-javascript'),
     asyncRequire = require('enb/lib/fs/async-require'),
     dropRequireCache = require('enb/lib/fs/drop-require-cache');
 
 module.exports = require('enb/lib/build-flow.js').create()
     .name('i18n-merge-keysets')
-    .defineRequiredOption('lang')
-    .useDirList('i18n')
     .target('target', '?.keysets.{lang}.js')
-    .builder(function (langKeysetDirs) {
-        var lang = this._lang,
-            langJs = lang + '.js',
-            langKeysetFiles = [].concat.apply([], langKeysetDirs.map(function (dir) {
-                return dir.files;
-            })).filter(function (fileInfo) {
-                return fileInfo.name === langJs;
-            }),
-            node = this.node,
+    .defineRequiredOption('lang')
+    .useFileList(['i18n.js'])
+    .useDirList(['i18n'])
+    .builder(function (i18nFiles, i18nDirs) {
+        var node = this.node,
             cache = node.getNodeCache(this._target),
-            result = {};
+            lang = this._lang,
+            langname = lang + '.js',
+            result = {},
+            files = [].concat(i18nFiles).concat(i18nDirs
+                .reduce(function (prev, dir) {
+                    return prev.concat(dir.files);
+                }, [])
+                .filter(function (file) {
+                    return [langname, 'all.js'].indexOf(file.name) > -1;
+                })
+            );
 
-        return vow.all(langKeysetFiles.map(function (keysetFile) {
-                var filename = keysetFile.fullname,
-                    basename = keysetFile.name,
-                    cacheKey = 'keyset-file-' + basename,
-                    promise;
+        return vow.all(files.map(function (file) {
+            var filename = file.fullname,
+                basename = file.name,
+                cacheKey = 'keyset-file-' + basename,
+                promise;
 
-                if (cache.needRebuildFile(cacheKey, filename)) {
-                    dropRequireCache(require, filename);
-                    promise = asyncRequire(filename)
-                        .then(function (keysets) {
-                            cache.cacheFileInfo(cacheKey, filename);
+            if (cache.needRebuildFile(cacheKey, filename)) {
+                dropRequireCache(require, filename);
+                promise = asyncRequire(filename)
+                    .then(function (keysets) {
+                        cache.cacheFileInfo(cacheKey, filename);
 
-                            return keysets;
+                        return keysets;
+                    });
+            } else {
+                promise = asyncRequire(filename);
+            }
+
+            return promise.then(function (keysets) {
+                    Object.keys(keysets).forEach(function (scope) {
+                        var keyset = keysets[scope];
+
+                        result[scope] || (result[scope] = {});
+
+                        Object.keys(keyset).forEach(function (name) {
+                            result[scope][name] = keyset[name];
                         });
-                } else {
-                    promise = asyncRequire(filename);
-                }
-
-                promise.then(function (keysets) {
-                    if (lang === 'all') { // XXX: Why the hell they break the pattern?
-                        keysets = keysets.all || {};
-                    }
-                    Object.keys(keysets).forEach(function (keysetName) {
-                        var keyset = keysets[keysetName];
-                        result[keysetName] = (result[keysetName] || {});
-                        if (typeof keyset !== 'string') {
-                            Object.keys(keyset).forEach(function (keyName) {
-                                result[keysetName][keyName] = keyset[keyName];
-                            });
-                        } else {
-                            result[keysetName] = keyset;
-                        }
                     });
                 });
             }))
             .then(function () {
-                return 'module.exports = ' + JSON.stringify(result) + ';';
+                return 'module.exports = ' + serialize(result) + ';';
             });
     })
     .createTech();
