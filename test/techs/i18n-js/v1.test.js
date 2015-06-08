97f03df468412bf4320be7ded643ac7aaa07a6e6
tormozz48 <tormozz48@gmail.com>
2015-06-08 17:10:38 +0300

    Split tests for i18n-js tech into different files by core versions criteria

--- test/techs/i18n-js/v1.test.js
+++ test/techs/i18n-js/v1.test.js
@@ -4,14 +4,13 @@ var fs = require('fs'),
     serializeJS = require('serialize-javascript'),
     TestNode = require('enb/lib/test/mocks/test-node'),
     dropRequireCache = require('enb/lib/fs/drop-require-cache'),
-    Tech = require('../../techs/i18n-js'),
-    corev1,
-    corev2 = require('../fixtures/bem-core-v3/common.blocks/i18n/i18n.i18n.js').i18n.i18n;
+    Tech = require('../../../techs/i18n-js'),
+    core;
 
-describe('i18n-js', function () {
+describe('i18n-js v1', function () {
     before(function () {
         var p = './test/fixtures/bem-core/common.blocks/i-bem/__i18n/i-bem__i18n.i18n/core.js';
-        corev1 = fs.readFileSync(path.resolve(p), { encoding: 'utf-8' });
+        core = fs.readFileSync(path.resolve(p), { encoding: 'utf-8' });
     });
 
     afterEach(function () {
@@ -28,24 +27,10 @@ describe('i18n-js', function () {
             });
     });
 
-    it('must throw err if i18n is not function', function () {
-        var keysets = {
-            i18n: {
-                i18n: 'val'
-            }
-        };
-
-        return build(keysets)
-            .fail(function (err) {
-                err.must.a(Error);
-                err.message.must.be('Core of i18n is not found!');
-            });
-    });
-
     it('must return value', function () {
         var keysets = {
-            i18n: {
-                i18n: corev2
+            all: {
+                '': core
             },
             scope: {
                 key: 'val'
@@ -58,83 +43,29 @@ describe('i18n-js', function () {
             });
     });
 
-    it('must build fake key if keysets is empty', function () {
+    it('must get valid *.lang from *.keyset file', function () {
         var keysets = {
-            i18n: {
-                i18n: corev2
-            }
+            all: { '': core },
+            scope1: { key11: 'val11', key12: 'val12' },
+            scope2: { key21: 'val21' }
+        };
+        return build(keysets)
+            .then(function (i18n) {
+                i18n('scope1', 'key11').must.be('val11');
+                i18n('scope1', 'key12').must.be('val12');
+                i18n('scope2', 'key21').must.be('val21');
+            });
+    });
+
+    it('must get valid *.lang from empty *.keyset file (only core)', function () {
+        var keysets = {
+            all: { '': core }
         };
 
         return build(keysets)
             .then(function (i18n) {
-                i18n('scope', 'key').must.be('scope:key');
+                i18n('scope', 'key').must.be('');
             });
-    });
-
-    it('must build key by params', function () {
-        var keysets = {
-            i18n: {
-                i18n: corev2
-            },
-            scope: {
-                key: function (params) {
-                    return params.join();
-                }
-            }
-        };
-
-        return build(keysets)
-            .then(function (i18n) {
-                i18n('scope', 'key', ['p1', 'p2']).must.be('p1,p2');
-            });
-    });
-
-    it('must provide i18n instance to function', function () {
-        var keysets = {
-            i18n: {
-                i18n: corev2
-            },
-            'scope-1': {
-                key: 'val'
-            },
-            'scope-2': {
-                key: function (params, i18n) {
-                    return i18n(params.scope, params.key);
-                }
-            }
-        };
-
-        return build(keysets)
-            .then(function (i18n) {
-                i18n('scope-2', 'key', { scope: 'scope-1', key: 'key' }).must.be('val');
-            });
-    });
-
-    describe('corev1', function () {
-        it('must get valid *.lang from *.keyset file', function () {
-            var keysets = {
-                    all: { '': corev1 },
-                    scope1: { key11: 'val11', key12: 'val12' },
-                    scope2: { key21: 'val21' }
-                };
-            return build(keysets)
-                .then(function (i18n) {
-                    i18n('scope1', 'key11').must.be('val11');
-                    i18n('scope1', 'key12').must.be('val12');
-                    i18n('scope2', 'key21').must.be('val21');
-                });
-        });
-
-        it('must get valid *.lang from empty *.keyset file (only core)', function () {
-            var keysets = {
-                all: { '': corev1 }
-            };
-
-            return build(keysets)
-                .then(function (i18n) {
-                    i18n('scope', 'key').must.be('');
-                });
-        });
     });
 
     describe('cache', function () {
@@ -145,7 +76,7 @@ describe('i18n-js', function () {
                 bundle: {
                     'bundle.keysets.lang.js': mock.file({
                         content: serialize({
-                            i18n: { i18n: corev2 },
+                            all: { '': core },
                             scope: { key: 'val' }
                         }),
                         mtime: time
@@ -166,7 +97,7 @@ describe('i18n-js', function () {
                 bundle: {
                     'bundle.keysets.lang.js': mock.file({
                         content: serialize({
-                            i18n: { i18n: corev2 },
+                            all: { '': core },
                             scope: { key: 'val2' }
                         }),
                         mtime: time
@@ -185,7 +116,7 @@ describe('i18n-js', function () {
                 bundle: {
                     'bundle.keysets.lang.js': mock.file({
                         content: serialize({
-                            i18n: { i18n: corev2 },
+                            all: { '': core },
                             scope: { key: 'val' }
                         }),
                         mtime: new Date(1)
@@ -206,7 +137,7 @@ describe('i18n-js', function () {
                 bundle: {
                     'bundle.keysets.lang.js': mock.file({
                         content: serialize({
-                            i18n: { i18n: corev2 },
+                            all: { '': core },
                             scope: { key: 'val2' }
                         }),
                         mtime: new Date(2)
@@ -240,3 +171,4 @@ function build(keysets) {
 function serialize(js) {
     return 'module.exports = ' + serializeJS(js) + ';';
 }
+
---
+++ test/techs/i18n-js/v2.test.js
@@ -0,0 +1,209 @@
+var path = require('path'),
+    mock = require('mock-fs'),
+    serializeJS = require('serialize-javascript'),
+    TestNode = require('enb/lib/test/mocks/test-node'),
+    dropRequireCache = require('enb/lib/fs/drop-require-cache'),
+    Tech = require('../../../techs/i18n-js'),
+    core = require('../../fixtures/bem-core-v3/common.blocks/i18n/i18n.i18n.js').i18n.i18n;
+
+describe('i18n-js v2', function () {
+    afterEach(function () {
+        mock.restore();
+    });
+
+    it('must throw err if i18n is not found', function () {
+        var keysets = {};
+
+        return build(keysets)
+            .fail(function (err) {
+                err.must.a(Error);
+                err.message.must.be('Core of i18n is not found!');
+            });
+    });
+
+    it('must throw err if i18n is not function', function () {
+        var keysets = {
+            i18n: {
+                i18n: 'val'
+            }
+        };
+
+        return build(keysets)
+            .fail(function (err) {
+                err.must.a(Error);
+                err.message.must.be('Core of i18n is not found!');
+            });
+    });
+
+    it('must return value', function () {
+        var keysets = {
+            i18n: {
+                i18n: core
+            },
+            scope: {
+                key: 'val'
+            }
+        };
+
+        return build(keysets)
+            .then(function (i18n) {
+                i18n('scope', 'key').must.be('val');
+            });
+    });
+
+    it('must build fake key if keysets is empty', function () {
+        var keysets = {
+            i18n: {
+                i18n: core
+            }
+        };
+
+        return build(keysets)
+            .then(function (i18n) {
+                i18n('scope', 'key').must.be('scope:key');
+            });
+    });
+
+    it('must build key by params', function () {
+        var keysets = {
+            i18n: {
+                i18n: core
+            },
+            scope: {
+                key: function (params) {
+                    return params.join();
+                }
+            }
+        };
+
+        return build(keysets)
+            .then(function (i18n) {
+                i18n('scope', 'key', ['p1', 'p2']).must.be('p1,p2');
+            });
+    });
+
+    it('must provide i18n instance to function', function () {
+        var keysets = {
+            i18n: {
+                i18n: core
+            },
+            'scope-1': {
+                key: 'val'
+            },
+            'scope-2': {
+                key: function (params, i18n) {
+                    return i18n(params.scope, params.key);
+                }
+            }
+        };
+
+        return build(keysets)
+            .then(function (i18n) {
+                i18n('scope-2', 'key', { scope: 'scope-1', key: 'key' }).must.be('val');
+            });
+    });
+
+    describe('cache', function () {
+        it('must get result from cache', function () {
+            var time = new Date(1);
+
+            mock({
+                bundle: {
+                    'bundle.keysets.lang.js': mock.file({
+                        content: serialize({
+                            i18n: { i18n: core },
+                            scope: { key: 'val' }
+                        }),
+                        mtime: time
+                    })
+                }
+            });
+
+            var bundle = new TestNode('bundle'),
+                cache = bundle.getNodeCache('bundle.lang.lang.js'),
+                basename = 'bundle.keysets.lang.js',
+                filename = path.resolve('bundle', basename);
+
+            dropRequireCache(require, filename);
+            require(filename);
+            cache.cacheFileInfo('keysets-file-' + basename, filename);
+
+            mock({
+                bundle: {
+                    'bundle.keysets.lang.js': mock.file({
+                        content: serialize({
+                            i18n: { i18n: core },
+                            scope: { key: 'val2' }
+                        }),
+                        mtime: time
+                    })
+                }
+            });
+
+            return bundle.runTechAndRequire(Tech, { lang: 'lang' })
+                .spread(function (i18n) {
+                    i18n('scope', 'key').must.be('val');
+                });
+        });
+
+        it('must ignore outdated cache', function () {
+            mock({
+                bundle: {
+                    'bundle.keysets.lang.js': mock.file({
+                        content: serialize({
+                            i18n: { i18n: core },
+                            scope: { key: 'val' }
+                        }),
+                        mtime: new Date(1)
+                    })
+                }
+            });
+
+            var bundle = new TestNode('bundle'),
+                cache = bundle.getNodeCache('bundle.lang.lang.js'),
+                basename = 'bundle.keysets.lang.js',
+                filename = path.resolve('bundle', basename);
+
+            dropRequireCache(require, filename);
+            require(filename);
+            cache.cacheFileInfo('keysets-file-' + basename, filename);
+
+            mock({
+                bundle: {
+                    'bundle.keysets.lang.js': mock.file({
+                        content: serialize({
+                            i18n: { i18n: core },
+                            scope: { key: 'val2' }
+                        }),
+                        mtime: new Date(2)
+                    })
+                }
+            });
+
+            return bundle.runTechAndRequire(Tech, { lang: 'lang' })
+                .spread(function (i18n) {
+                    i18n('scope', 'key').must.be('val2');
+                });
+        });
+    });
+});
+
+function build(keysets) {
+    mock({
+        bundle: {
+            'bundle.keysets.lang.js': serialize(keysets)
+        }
+    });
+
+    var bundle = new TestNode('bundle');
+
+    return bundle.runTechAndRequire(Tech, { lang: 'lang' })
+        .spread(function (i18n) {
+            return i18n;
+        });
+}
+
+function serialize(js) {
+    return 'module.exports = ' + serializeJS(js) + ';';
+}
+
